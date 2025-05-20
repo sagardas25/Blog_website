7cf3a89be12d298ef39badf0188888ec9a68736e
sagardas25 <sagar.xyz05@gmail.com>
2025-05-21 00:50:51 +0530

    initial setup done

---
+++ Blog-web/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      <h1 className="p-3 bg-amber-900">Hellooo people</h1>
+    </>
+  );
+}
+
+export default App;
